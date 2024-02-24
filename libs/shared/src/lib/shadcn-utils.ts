90b49067cdc21335a70e4528b006e6bca242f07b
Nathan Jones <nathanjones314@gmail.com>
2024-02-23 23:41:23 -0600

    chore: scaffold a lib for ui components

---
+++ libs/shared/src/lib/shadcn-utils.ts
@@ -0,0 +1,8 @@
+// shadcn helper
+
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
