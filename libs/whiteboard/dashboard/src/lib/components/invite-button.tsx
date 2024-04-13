122bdd1653338235a178a59894190161508560aa
Nathan Jones <nathanjones314@gmail.com>
2024-04-13 17:49:39 -0500

    feat: search input and invite button

---
+++ libs/whiteboard/dashboard/src/lib/components/invite-button.tsx
@@ -0,0 +1,24 @@
+import { OrganizationProfile } from '@clerk/nextjs';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from '@nextjs-practice/shared';
+import { Plus } from 'lucide-react';
+
+export function InviteButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus className="h-4 w-4 mr-2" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+        <OrganizationProfile />
+      </DialogContent>
+    </Dialog>
+  );
+}
--- libs/whiteboard/dashboard/src/lib/components/navbar.tsx
+++ libs/whiteboard/dashboard/src/lib/components/navbar.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import { UserButton } from '@clerk/nextjs';
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from '@clerk/nextjs';
 import { ThemeModeToggle } from '@shared/components/theme';
+import { InviteButton } from './invite-button';
 import { SearchInput } from './search-input';
 
 export function Navbar() {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
+
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: 'flex justify-center items-center w-full max-w-[376px]',
+              organizationSwitcherTrigger:
+                'p-1.5 w-full rounded-lg border border-solid border-gray-300 dark:border-gray-700 justify-between dark:text-white',
+            },
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
       <ThemeModeToggle />
       <UserButton />
     </div>
