3960e0ddd6025216fdd1af13b400815f346b9f6e
Nathan Jones <nathanjones314@gmail.com>
2024-04-13 00:31:58 -0500

    style: logo tweaks

--- apps/whiteboard/src/components/auth/loading.tsx
+++ apps/whiteboard/src/components/auth/loading.tsx
@@ -4,10 +4,10 @@ export function Loading() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       <Image
-        src="/logo.png"
+        src="/logo-sm.png"
         alt="logo"
-        width={480}
-        height={480}
+        width={180}
+        height={180}
         className="animate-pulse duration-700"
       />
     </div>
